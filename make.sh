7dda66e01844a3396b02475a7e4096a9dea07c6e
Marvin Genser <marvin@genser.me>
2023-04-04 01:14:25 +0200

    create zip file

--- make.sh
+++ make.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Download the Alpine Linux image for Raspberry Pi 3
+# Download the Alpine Linux image for Raspberry Pi 4
 wget https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/aarch64/alpine-rpi-3.17.3-aarch64.tar.gz
 
 # Extract the contents of the downloaded archive
-tar -zxvf alpine-rpi-3.17.3-aarch64.tar.gz
+mkdir -p /unziped/alpine-rpi-3.17.3-aarch64/
+tar -zxvf alpine-rpi-3.17.3-aarch64.tar.gz -C /unziped/alpine-rpi-3.17.3-aarch64/
+
+# Download the headless modification and add it to the FS
+wget https://github.com/m-genser/alpine-linux-headless-mod/releases/download/main/headless.apkovl.tar.gz
+mv headless.apkovl.tar.gz /unziped/alpine-rpi-3.17.3-aarch64/headless.apkovl.tar.gz
 
 # Create a ZIP archive of the extracted files
-zip -r ./alpine-rpi-3.17.3-aarch64.zip ./alpine-rpi-3.17.3-aarch64/
+zip -r alpine-rpi-3.17.3-aarch64.zip /unziped/alpine-rpi-3.17.3-aarch64/
